b863b06bfc5f90f511ef4d98ebaa814cc96d7bdd
Aidan Drockman <81786841+AidanDrockman@users.noreply.github.com>
2021-04-07 10:37:17 +1000

    Added descriptions to User methods.

--- src/Shared/User.java
+++ src/Shared/User.java
@@ -10,6 +10,13 @@ public class User {
     private AccountType accountType;
     private String organisationalType;
 
+    /**
+     *
+     * @param username Username of the user
+     * @param password Password of the user
+     * @param accountType Account type of the use
+     * @param organisationalUnit Organisational Unit that the user belongs to
+     */
     public User(String username, String password, AccountType accountType, String organisationalUnit) {
         this.username = username;
         this.password = password;
@@ -17,18 +24,34 @@ public class User {
         this.organisationalType = organisationalUnit;
     }
 
+    /**
+     *
+     * @return username of the User
+     */
     public String GetUsername() {
         return username;
     }
 
+    /**
+     *
+     * @return password of the User
+     */
     public String GetPassword() {
         return password;
     }
 
+    /**
+     *
+     * @return account type of the User
+     */
     public AccountType GetAccountType() {
         return accountType;
     }
 
+    /**
+     *
+     * @return organisational unit of the User
+     */
     public String GetOrganisationalUnit() {
         return organisationalType;
     }
