fbc39d0d80146a0ed8012add7d780c940f795bc5
DeclanBarrett <declanbarre77@gmail.com>
2021-04-20 00:38:34 +1000

    Functioning Reset Password Screen without the backend - login is with the name "ANDY" and the Password "123". Can be better by using user object etc

--- src/Client/Controller/ResetController.java
+++ src/Client/Controller/ResetController.java
@@ -7,8 +7,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -26,6 +28,11 @@ public class ResetController implements Initializable {
     @FXML
     private PasswordField NewPassword2Text;
 
+    @FXML
+    private Label ErrorText;
+
+    private boolean passwordsMatch;
+
     // method allowing the login button to progress user to the user login page.
     public void ResetPassword (ActionEvent ResetPassword) throws IOException {
 
@@ -33,21 +40,60 @@ public class ResetController implements Initializable {
         System.out.println(OldPasswordText.getText());
         System.out.println(NewPasswordText.getText());
         System.out.println(NewPassword2Text.getText());
+        System.out.println(passwordsMatch);
 
-        Parent loginView;
-        loginView = FXMLLoader.load(getClass().getResource("login.fxml"));
-        Scene loginViewScene = new Scene(loginView);
+        if (passwordsMatch) {
+            if (CheckExistingPassword(UsernameTextArea.getText(), OldPasswordText.getText())) {
+                UpdatePassword();
 
-        Stage window = (Stage)((Node)ResetPassword.getSource()).getScene().getWindow();
+                Parent loginView;
+                loginView = FXMLLoader.load(getClass().getResource("login.fxml"));
+                Scene loginViewScene = new Scene(loginView);
 
-        window.setScene(loginViewScene);
-        window.show();
+                Stage window = (Stage)((Node)ResetPassword.getSource()).getScene().getWindow();
+
+                window.setScene(loginViewScene);
+                window.show();
+            } else {
+                ErrorText.setText("USERNAME OR PASSWORD INCORRECT");
+            }
+        }
 
 
+
+
+    }
+
+    public void CheckPasswordDifference(KeyEvent CheckPasswordDifference) throws IOException {
+        if (!NewPasswordText.getText().equals(NewPassword2Text.getText())) {
+            ErrorText.setText("PASSWORDS DO NOT MATCH");
+            passwordsMatch = false;
+            System.out.println(passwordsMatch);
+        } else {
+            ErrorText.setText("ALL GOOD");
+            passwordsMatch = true;
+            System.out.println(passwordsMatch);
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    private boolean CheckExistingPassword(String username, String password) {
+        /**TODO GO TO THE DATBASE AND CHECK
+         *
+         */
+
+        return username.equals("ANDY") && password.equals("123");
+    }
+
+    private void UpdatePassword() {
+        /**TODO GO TO THE DATBASE AND UPDATE
+         *
+         */
+    }
+
+
 }
