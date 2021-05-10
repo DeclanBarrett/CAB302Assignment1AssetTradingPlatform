267a7ea119d7467d9121b99d071a1bf7b3df06ae
Brad <n10632999@qut.edu.au>
2021-05-10 12:59:27 +1000

    Fixed issues with GUI not loading, fixed pathing as well.

--- src/App_Start/ClientStart.java
+++ src/App_Start/ClientStart.java
@@ -1,13 +1,38 @@
 package App_Start;
 
 
-import Controllers.FrontEnd.ClientSocket;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class ClientStart
+public class ClientStart extends Application
 {
     public static void main(String[] args)
     {
-        ClientSocket clientSocket = new ClientSocket();
-        clientSocket.startClient();
+//        ClientSocket clientSocket = new ClientSocket();
+//        clientSocket.startClient();
+
+        launch(args);
     }
-}
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        stage.setTitle("Unnamed Client Organisation Trading System");
+
+        Parent root;
+
+        // Admin
+        //root = FXMLLoader.load(getClass().getResource("/Client/FXMLPages/Admin/AdminScreen.fxml"));
+        // User
+        //root = FXMLLoader.load(getClass().getResource("/Client/FXMLPages/User/UserScreen.fxml"));
+        // Login
+        root = FXMLLoader.load(getClass().getResource("/Views/Login/Login.fxml"));
+        // Reset
+        //root = FXMLLoader.load(getClass().getResource("/Client/FXMLPages/PasswordReset.fxml"));
+
+        stage.setScene(new Scene(root));
+        stage.show();
+    } }
