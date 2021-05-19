a421625004b4a4bbb4f79782fa1d88876b639186
Brad <n10632999@qut.edu.au>
2021-05-19 16:21:35 +1000

    Issues with Creating SQL Tables.
    SolN: Create the tables in SQL order, i.e. tables need to be present in DB before creating Foreign keys.
    
    Notes:
    Had to create DB Manually

--- src/App_Start/ServerStart.java
+++ src/App_Start/ServerStart.java
@@ -1,5 +1,7 @@
 package App_Start;
 
+import Models.SQL;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,6 +11,10 @@ public class ServerStart
     public static void main(String[] args) throws SQLException
     {
         Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/trading_platform", "root", "HelloWorld11");
+        SQL sql = new SQL(connection);
+        sql.populateDatabase();
         connection.close();
+
+
     }
 }
--- src/Models/SQL.java
+++ src/Models/SQL.java
@@ -1,21 +1,15 @@
 package Models;
 
-import Controllers.Backend.AccountType;
-import Controllers.Backend.NetworkObjects.*;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 /**
  * Creates and initialises SQL database. Features all table creation String statements
  */
-public class SQL {
+public class SQL
+{
     // Table and Database Creation
     public static final String CREATE_DATABASE =
             "CREATE DATABASE IF NOT EXISTS trading_platform;";
@@ -69,25 +63,42 @@ public class SQL {
                     ");";
 
     // Population of database via SQL Queries (based off Mock Socket data)
-    private static final String INSERT_NEW_USER1 = "INSERT INTO Users VALUES ('User 1', 'Sales', 'User', 'b717415eb5e699e4989ef3e2c4e9cbf7', '12345')";
-    private static final String INSERT_NEW_USER2 = "INSERT INTO Users VALUES ('User 2', 'Sales', 'User', 'b717415eb5e699e4989ef3e2c4e9cbf7', '12345')";
-    private static final String INSERT_NEW_USER3 = "INSERT INTO Users VALUES ('User 3', 'Finance', 'User', '8d421e892a47dff539f46142eb09e56b', '123456')";
-    private static final String INSERT_NEW_USER4 = "INSERT INTO Users VALUES ('User 4', 'Finance', 'User', 'b26b843656e6834822b83179b4297620', '123457')";
-    private static final String INSERT_NEW_USER5 = "INSERT INTO Users VALUES ('User 5', 'Finance', 'User', 'c3a4b61825259a74c26d49daa3e89312', '123458')";
-    private static final String INSERT_NEW_USER6 = "INSERT INTO Users VALUES ('User 6', 'Research', 'User', '2ec7484fa99bbaa7bdebe544f1f52f61', '123459')";
-    private static final String INSERT_NEW_USER7 = "INSERT INTO Users VALUES ('User 7', 'Research', 'User', 'ccab59bc481b2105a4dbdf3d30a66248', '123450')";
-    private static final String INSERT_NEW_USER8 = "INSERT INTO Users VALUES ('User 8', 'Research', 'User', 'aa3cae505478da19d13efa65bc8c71b3', '123451')";
-    private static final String INSERT_NEW_USER9 = "INSERT INTO Users VALUES ('User 9', 'Research', 'User', '43bf88d863f230f328c15ccf61d9d89d', '123452')";
+    private static final String INSERT_NEW_USER1 = "INSERT INTO Users VALUES ('User 1', 'Sales', 'User', " +
+            "'b717415eb5e699e4989ef3e2c4e9cbf7', '12345')";
+    private static final String INSERT_NEW_USER2 = "INSERT INTO Users VALUES ('User 2', 'Sales', 'User', " +
+            "'b717415eb5e699e4989ef3e2c4e9cbf7', '12345')";
+    private static final String INSERT_NEW_USER3 = "INSERT INTO Users VALUES ('User 3', 'Finance', 'User', " +
+            "'8d421e892a47dff539f46142eb09e56b', '123456')";
+    private static final String INSERT_NEW_USER4 = "INSERT INTO Users VALUES ('User 4', 'Finance', 'User', " +
+            "'b26b843656e6834822b83179b4297620', '123457')";
+    private static final String INSERT_NEW_USER5 = "INSERT INTO Users VALUES ('User 5', 'Finance', 'User', " +
+            "'c3a4b61825259a74c26d49daa3e89312', '123458')";
+    private static final String INSERT_NEW_USER6 = "INSERT INTO Users VALUES ('User 6', 'Research', 'User', " +
+            "'2ec7484fa99bbaa7bdebe544f1f52f61', '123459')";
+    private static final String INSERT_NEW_USER7 = "INSERT INTO Users VALUES ('User 7', 'Research', 'User', " +
+            "'ccab59bc481b2105a4dbdf3d30a66248', '123450')";
+    private static final String INSERT_NEW_USER8 = "INSERT INTO Users VALUES ('User 8', 'Research', 'User', " +
+            "'aa3cae505478da19d13efa65bc8c71b3', '123451')";
+    private static final String INSERT_NEW_USER9 = "INSERT INTO Users VALUES ('User 9', 'Research', 'User', " +
+            "'43bf88d863f230f328c15ccf61d9d89d', '123452')";
 
-    private static final String INSERT_NEW_USER_DECLAN = "INSERT INTO Users VALUES ('Declan Testing', 'Admin', 'SystemAdmin', '802b492fc1d1fe592090399c1ca3b56a', '12346')";
-    private static final String INSERT_NEW_USER_AIDEN = "INSERT INTO Users VALUES ('Aiden Testing', 'Admin', 'SystemAdmin', '086e1b7e1c12ba37cd473670b3a15214', '123456')";
-    private static final String INSERT_NEW_USER_BRAD = "INSERT INTO Users VALUES ('Brad Testing', 'Admin', 'SystemAdmin', '086e1b7e1c12ba37cd473670b3a15214', '123456')";
-    private static final String INSERT_NEW_USER_ETHAN = "INSERT INTO Users VALUES ('Ethan Testing', 'Admin', 'SystemAdmin', '086e1b7e1c12ba37cd473670b3a15214', '123456')";
+    private static final String INSERT_NEW_USER_DECLAN = "INSERT INTO Users VALUES ('Declan Testing', 'Admin', 'SystemAdmin', " +
+            "'802b492fc1d1fe592090399c1ca3b56a', '12346')";
+    private static final String INSERT_NEW_USER_AIDEN = "INSERT INTO Users VALUES ('Aiden Testing', 'Admin', 'SystemAdmin', " +
+            "'086e1b7e1c12ba37cd473670b3a15214', '123456')";
+    private static final String INSERT_NEW_USER_BRAD = "INSERT INTO Users VALUES ('Brad Testing', 'Admin', 'SystemAdmin', " +
+            "'086e1b7e1c12ba37cd473670b3a15214', '123456')";
+    private static final String INSERT_NEW_USER_ETHAN = "INSERT INTO Users VALUES ('Ethan Testing', 'Admin', 'SystemAdmin', " +
+            "'086e1b7e1c12ba37cd473670b3a15214', '123456')";
 
-    private static final String INSERT_NEW_USER10 = "INSERT INTO Users VALUES ('User 10', 'Sales', 'UnitLeader', '579d9ec9d0c3d687aaa91289ac2854e4', '123452')";
-    private static final String INSERT_NEW_USER11 = "INSERT INTO Users VALUES ('User 11', 'Finance', 'UnitLeader', '086e1b7e1c12ba37cd473670b3a15214', '123452')";
-    private static final String INSERT_NEW_USER12 = "INSERT INTO Users VALUES ('User 12', 'Research', 'UnitLeader', '086e1b7e1c12ba37cd473670b3a15214', '123452')";
-    private static final String INSERT_NEW_USER13 = "INSERT INTO Users VALUES ('User 13', 'Research', 'UnitLeader', '086e1b7e1c12ba37cd473670b3a15214', '123452')";
+    private static final String INSERT_NEW_USER10 = "INSERT INTO Users VALUES ('User 10', 'Sales', 'UnitLeader', " +
+            "'579d9ec9d0c3d687aaa91289ac2854e4', '123452')";
+    private static final String INSERT_NEW_USER11 = "INSERT INTO Users VALUES ('User 11', 'Finance', 'UnitLeader', " +
+            "'086e1b7e1c12ba37cd473670b3a15214', '123452')";
+    private static final String INSERT_NEW_USER12 = "INSERT INTO Users VALUES ('User 12', 'Research', 'UnitLeader', " +
+            "'086e1b7e1c12ba37cd473670b3a15214', '123452')";
+    private static final String INSERT_NEW_USER13 = "INSERT INTO Users VALUES ('User 13', 'Research', 'UnitLeader', " +
+            "'086e1b7e1c12ba37cd473670b3a15214', '123452')";
 
     private static final String INSERT_NEW_ORGANISATIONAL_UNIT_SALES = "INSERT INTO OrganisationalUnit VALUES ('Sales', 3000.50)";
     private static final String INSERT_NEW_ORGANISATIONAL_UNIT_FINANCE = "INSERT INTO OrganisationalUnit VALUES ('Finance', 100)";
@@ -101,18 +112,16 @@ public class SQL {
     private PreparedStatement addOrganisationalUnit;
 
     // This function currently 'works' for the users tables
-    public SQL(Connection connection) {
+    public SQL(Connection connection)
+    {
         this.connection = connection;
-        CreateTables();
     }
-
-    private void CreateTables() {}
-
 
 
     // Create and Populate prepare statements in here
-    public SQL() {
-        connection = DatabaseConnection.getInstance();
+    public void populateDatabase()
+    {
+        //connection = DatabaseConnection.getInstance(); //TODO: This will cause an exception if not Connection is passed to Constructor!
         try {
             Statement createUserTableStatement = connection.createStatement();
             Statement createOrgHasQuantityTableStatement = connection.createStatement();
@@ -164,8 +173,10 @@ public class SQL {
             ex.printStackTrace();
         }
     }
+
     // Populate functions (Currently adding user and organisational unit)
-    public void AddUser(String username, String orgUnit, String accType, String hashedPW, String salt) {
+    public void AddUser(String username, String orgUnit, String accType, String hashedPW, String salt)
+    {
         try {
             AddUser.setString(1, username);
             AddUser.setString(2, orgUnit);
@@ -180,7 +191,9 @@ public class SQL {
             throwables.printStackTrace();
         }
     }
-    public void AddOrganisationalUnit(String orgUnitName, Integer amountCredits) {
+
+    public void AddOrganisationalUnit(String orgUnitName, Integer amountCredits)
+    {
         try {
             addOrganisationalUnit.setString(1, orgUnitName);
             addOrganisationalUnit.setInt(2, amountCredits);
